3077473329fefcc32e39f7e27b1303134e603d97
tamango92 <ar.arkhipov@gmail.com>
2018-07-23 13:09:44 +0300

    mongo 3.6 support - update version on mongoose

--- libs/config.js
+++ libs/config.js
@@ -1,5 +1,5 @@
 var config = {
-    port: process.env.port || 1337,
+    port: process.env.PORT || 1337,
     jwtSecret: process.env.JWT_SECRET || 'SECRET_SHHHHHH',
     mongouri: process.env.MONGO_URI || 'mongodb://localhost/lingvoman'
 };
